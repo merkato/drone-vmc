1957fb6c6b9e652bad99faa8b82a2c662b57c3e8
Tomasz Nycz <tomasz.merkato@gmail.com>
2026-01-09 20:33:31 +0100

    zepsute

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ from pathlib import Path
 # Środowisko i domena
 DOMAIN = os.getenv('DOMAIN', 'localhost')
 STORAGE_SECRET = os.getenv('STORAGE_SECRET', 'PesaToNajgorszyProducentTaboruNaSwiecie')
+DATABASE_URL = "sqlite:////app/data/system.db"
 
 # Ścieżki i serwery
 RECORDINGS_DIR = Path("/recordings")
